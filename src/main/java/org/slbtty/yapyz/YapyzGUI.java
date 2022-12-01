af81961d9a666c8b8a4670c938a841bdd02a7f50
shenleban tongying <shenlebantongying@gmail.com>
2022-12-01 10:05:07 -0500

    ui: add basic interactions

--- src/main/java/org/slbtty/yapyz/YapyzGUI.java
+++ src/main/java/org/slbtty/yapyz/YapyzGUI.java
@@ -5,16 +5,99 @@ import com.formdev.flatlaf.FlatIntelliJLaf;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 
 public class YapyzGUI extends JFrame {
 
+    // platforms
+    final static Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+    final static UrlHandler urlHandler = new UrlHandler();
     public static IndexSettings indexSettings;
-    public  Indexer indexer;
+    private static JDialog pathDialog;
+    public Indexer indexer;
+    // temp variable to hold table's selected row path
+    private String p_selected_row_path;
 
-    public void initMainWindow()  {
+    public YapyzGUI() {
+
+        // macOS things
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
+
+        setTitle("Yapyz");
+        setSize(600, 800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        FlatIntelliJLaf.setup();
+        initMainWindow();
+        initSettingDialog();
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            var mainWindow = new YapyzGUI();
+            mainWindow.setVisible(true);
+        });
+    }
+
+    private void initSettingDialog() {
+        pathDialog = new JDialog(this, "Index Paths", true);
+        pathDialog.setSize(new Dimension(300, 500));
+
+        var addRemoveBar = Box.createHorizontalBox();
+        var mainbox = pathDialog.getContentPane();
+
+        var addBtn = new JButton("add");
+        var removeBtn = new JButton("remove");
+        var pathModel = new DefaultListModel<String>();
+        var pathList = new JList<String>(pathModel);
+        var okBtn = new JButton("ok");
+
+        addRemoveBar.add(addBtn);
+        addRemoveBar.add(removeBtn);
+
+        mainbox.add(addRemoveBar, BorderLayout.NORTH);
+        mainbox.add(pathList, BorderLayout.CENTER);
+        mainbox.add(okBtn, BorderLayout.SOUTH);
+
+        // Populate the list from user's current settings
+        // todo: this can be write better
+        for (var x : indexSettings.indexPaths) {
+            pathModel.addElement(x);
+        }
+        //
+
+        // Event binding
+        final JFileChooser fc = new JFileChooser();
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        addBtn.addActionListener(e -> {
+            int ret = fc.showOpenDialog(addBtn);
+            if (ret == JFileChooser.APPROVE_OPTION) {
+                String selected_dir = fc.getSelectedFile().getAbsolutePath();
+                pathModel.addElement(selected_dir);
+                indexSettings.addPath(selected_dir);
+            }
+        });
+
+        removeBtn.addActionListener(e -> {
+
+            indexSettings.removePath(pathList.getSelectedValue());
+
+            // Removal of the selected must be the last step, or you will get invalid values
+            pathModel.remove(pathList.getSelectedIndex());
+        });
+
+        okBtn.addActionListener(e -> {
+            indexSettings.saveToDisk();
+            pathDialog.setVisible(false);
+        });
+
+    }
+
+    public void initMainWindow() {
         indexSettings = new IndexSettings();
         try {
             indexer = new Indexer();
@@ -27,8 +110,14 @@ public class YapyzGUI extends JFrame {
 
         var indexControlMenu = new JMenu("Index Control");
         var rebuildIndexMenu = new JMenuItem("Rebuild Index");
+        var configIndexMenu = new JMenuItem("Index Config");
 
         indexControlMenu.add(rebuildIndexMenu);
+        indexControlMenu.add(configIndexMenu);
+
+        configIndexMenu.addActionListener(e -> {
+            pathDialog.setVisible(true);
+        });
 
         menuBar.add(indexControlMenu);
 
@@ -41,10 +130,7 @@ public class YapyzGUI extends JFrame {
 
         var searchBtn = new JButton("Search");
         var searchTextField = new JTextField();
-        searchTextField.setMaximumSize(
-            new Dimension(Integer.MAX_VALUE,searchTextField.getPreferredSize().height));
-
-
+        searchTextField.setMaximumSize(new Dimension(Integer.MAX_VALUE, searchTextField.getPreferredSize().height));
 
         searchBar.add(searchBtn);
         searchBar.add(searchTextField);
@@ -59,15 +145,29 @@ public class YapyzGUI extends JFrame {
 
         add(vbox);
 
-        setTitle("Yapyz");
-        setSize(600,800);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // tablePopup
+
+        var tableMenu = new JPopupMenu();
+        var table_copy_to_clipboard = new JMenuItem("Copy path to clipboard");
+        var table_open_file = new JMenuItem("Open file");
+        tableMenu.add(table_copy_to_clipboard);
+        tableMenu.add(table_open_file);
+
+        // Even register
+
+        table_copy_to_clipboard.addActionListener((ev) -> {
+            clipboard.setContents(new StringSelection(p_selected_row_path), null);
+        });
+
+        table_open_file.addActionListener((ev) -> {
+            urlHandler.open(p_selected_row_path);
+        });
 
         rebuildIndexMenu.addActionListener((ev -> {
             indexer.setPaths(indexSettings.indexPaths);
-            try{
+            try {
                 indexer.rebuildIndex();
-            } catch (IOException err){
+            } catch (IOException err) {
                 throw new RuntimeException(err);
             }
         }));
@@ -83,17 +183,16 @@ public class YapyzGUI extends JFrame {
 
         searchBtn.addActionListener(SearchAction);
         searchTextField.addActionListener(SearchAction);
-    }
 
-    public YapyzGUI() {
-        FlatIntelliJLaf.setup();
-        initMainWindow();
-    }
-
-    public static void main(String[] args) {
-        EventQueue.invokeLater(()->{
-            var mainWindow = new YapyzGUI();
-            mainWindow.setVisible(true);
+        table.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent ev) {
+                tableMenu.show(ev.getComponent(), ev.getX(), ev.getY());
+                int row = table.rowAtPoint(ev.getPoint());
+                if (row >= 0) {
+                    p_selected_row_path = (String) mainTableModel.getValueAt(row, 0);
+                }
+            }
         });
     }
 }
