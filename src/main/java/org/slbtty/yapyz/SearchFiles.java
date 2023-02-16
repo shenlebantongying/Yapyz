0b517892ecf581afc307c954425ac8763a7dacbf
shenleban tongying <shenlebantongying@gmail.com>
2023-02-15 19:47:41 -0500

    lucene: fix deprecation

--- src/main/java/org/slbtty/yapyz/SearchFiles.java
+++ src/main/java/org/slbtty/yapyz/SearchFiles.java
@@ -3,19 +3,14 @@ package org.slbtty.yapyz;
 
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.TermQuery;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.FSDirectory;
+import org.tinylog.Logger;
 
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.lucene.search.ScoreDoc;
-import org.tinylog.Logger;
 
 public class SearchFiles {
 
@@ -25,6 +20,7 @@ public class SearchFiles {
 
         try (org.apache.lucene.index.DirectoryReader reader = DirectoryReader.open(FSDirectory.open(indexStoragePath.toAbsolutePath()))) {
             var searcher = new IndexSearcher(reader);
+            var storedFields = reader.storedFields();
 
             Query query = new TermQuery(new Term("contents", queryString));
 
@@ -32,9 +28,8 @@ public class SearchFiles {
 
             List<ResultEntry> result = new ArrayList<>();
 
-
             for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
-                result.add(new ResultEntry(searcher.doc(scoreDoc.doc).get("path"),scoreDoc.score));
+                result.add(new ResultEntry(storedFields.document(scoreDoc.doc).get("path"), scoreDoc.score));
             }
 
             return result;
